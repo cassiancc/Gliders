38828904a4b7244437c5be9f65b4b229d6ef3b36
Cassian Godsted <cassian@godsted.com>
2025-10-06 20:14:43 -0400

    use world render events

--- fabric/src/main/java/net/venturecraft/gliders/fabric/FabricClientEvents.java
+++ fabric/src/main/java/net/venturecraft/gliders/fabric/FabricClientEvents.java
@@ -1,12 +1,56 @@
 package net.venturecraft.gliders.fabric;
 
+import com.mojang.math.Axis;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.CameraType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.player.Player;
 import net.venturecraft.gliders.VCGlidersClient;
+import net.venturecraft.gliders.client.layer.PlayerGliderLayer;
+import net.venturecraft.gliders.common.compat.trinket.CuriosTrinketsUtil;
+import net.venturecraft.gliders.common.item.GliderItem;
+import net.venturecraft.gliders.util.GliderUtil;
 
 public class FabricClientEvents {
 
     public static void init() {
         ClientLifecycleEvents.CLIENT_STARTED.register((minecraft -> VCGlidersClient.clientSetup()));
+        WorldRenderEvents.AFTER_TRANSLUCENT.register((worldRenderContext -> {
+            var posestack = worldRenderContext.matrixStack();
+            RenderBuffers bufferSource = Minecraft.getInstance().renderBuffers();
+
+            Player living = Minecraft.getInstance().player;
+            EntityRenderState state = Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(living).createRenderState();
+            var stack = CuriosTrinketsUtil.getInstance().getFirstFoundGlider(living);
+
+
+            if (Minecraft.getInstance().options.getCameraType() == CameraType.FIRST_PERSON && stack.getItem() instanceof GliderItem && GliderUtil.isGlidingWithActiveGlider(living) && state instanceof HumanoidRenderState humanoidRenderState) {
+                posestack.pushPose();
+                posestack.mulPose(Axis.XP.rotationDegrees(180));
+                posestack.mulPose(Axis.YP.rotationDegrees(living.getViewYRot(1F)));
+                posestack.translate(0, -2.4, -0.5);
+                posestack.scale(1.5F, 1.5F, 1.5F);
+
+                if (GliderItem.isSpaceGlider(stack)) {
+                    posestack.mulPose(Axis.YP.rotationDegrees(180));
+                    posestack.translate(0, -0.2, 0);
+                    PlayerGliderLayer.xWingModel.setupAnim(humanoidRenderState);
+                    PlayerGliderLayer.xWingModel.renderToBuffer(posestack, bufferSource.bufferSource().getBuffer(RenderType.entityCutoutNoCull(PlayerGliderLayer.getGliderTexture(stack))), LevelRenderer.getLightColor(living.level(), living.blockPosition()), OverlayTexture.NO_OVERLAY, -1);
+                } else {
+                    PlayerGliderLayer.gliderModel.setupAnim(humanoidRenderState);
+                    PlayerGliderLayer.gliderModel.renderToBuffer(posestack, bufferSource.bufferSource().getBuffer(RenderType.entityCutoutNoCull(PlayerGliderLayer.getGliderTexture(stack))), LevelRenderer.getLightColor(living.level(), living.blockPosition()), OverlayTexture.NO_OVERLAY, -1);
+                }
+                posestack.popPose();
+            }
+
+        }));
     }
 
 }
